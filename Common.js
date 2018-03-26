fda4f8ec36997bb440f578bfbfd7de46568b8491
shelly <yeejoy-tech@192.168.1.208>
2018-03-26 17:51:02 +0800

    add js

--- Common.js
+++ Common.js
@@ -1,3 +1,19 @@
 /**
  * Created by yeejoy-tech on 18/3/26.
  */
+window.onload=function() {//页面加载完毕之后再加载js
+    var oImg = document.getElementsByTagName("img");//通过标签获取元素结点
+    //旋转度数
+    var Deg = 360 / oImg.length;
+
+    //遍历 jquery forEach(数组方法)
+    //把oimg借用成array方法
+    Array.prototype.forEach.call(oImg, function (el, index) {
+        //console.log(index);
+        el.style.transform = "rotateY(" + Deg * index + "deg) translateZ(350px)";
+        el.style.zIndex = -index;
+        //动画 延时
+        el.style.transition = "transform 1s " + index * 0.1 + "s";
+
+    });
+}
